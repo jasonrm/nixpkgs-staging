17b6588a95df12d9ddea833f5b14add61cf576ed
Jason R. McNeil <jason@mcneil.dev>
2021-01-07 20:54:53 -0700

    imaptar: init at 1.4

--- default.nix
+++ default.nix
@@ -7,6 +7,7 @@
   redis-cell = pkgs.callPackage ./pkgs/development/libraries/redis-cell {};
 
   bupstash = pkgs.callPackage ./pkgs/tools/backup/bupstash {};
+  imaptar = pkgs.callPackage ./pkgs/tools/backup/imaptar {};
   imapbox = pkgs.callPackage ./pkgs/tools/backup/imapbox {};
 
   supervisord-go = pkgs.callPackage ./pkgs/tools/system/supervisord-go {};
---
+++ pkgs/tools/backup/imaptar/default.nix
@@ -0,0 +1,27 @@
+{ lib
+, buildGoModule
+, fetchFromGitHub
+}:
+buildGoModule rec {
+  pname = "imaptar";
+  version = "v1.4";
+
+  src = fetchFromGitHub {
+    owner = "XS4ALL";
+    repo = pname;
+    rev = version;
+    sha256 = "1zxbg1a96z3j0fjqp93akw0bw93hqklgwmdkm6ngc7fc98r0lalg";
+    # sha256 = lib.fakeSha256;
+  };
+
+  vendorSha256 = "0q8z6ym0sw5ajafymzkkbq0vzdjwwkrgr547xyai029m95vv1cgj";
+  # vendorSha256 = lib.fakeSha256;
+
+  meta = with lib; {
+    description = "utility to dump imap mailbox to a tarfile (maildir format)";
+    license = licenses.asl20;
+    homepage = "https://github.com/XS4ALL/imaptar";
+    # maintainers = with lib.maintainers; [ philandstuff rawkode ];
+    # platforms = platforms.darwin ++ platforms.linux;
+  };
+}
