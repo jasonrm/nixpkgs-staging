ac90b85912914ce5d63b5e7d6361bad362a7da32
Jason R. McNeil <jason@mcneil.dev>
2021-04-22 14:00:29 -0700

    bupstash: 0.6.4 -> 0.9.0

--- pkgs/tools/backup/bupstash/default.nix
+++ pkgs/tools/backup/bupstash/default.nix
@@ -3,24 +3,30 @@
 , rustPlatform
 , pkg-config
 , libsodium
+, libiconv
 }:
 rustPlatform.buildRustPackage rec {
   pname = "bupstash";
-  version = "v0.6.4";
+  version = "v0.9.0";
 
   src = fetchFromGitHub {
     owner = "andrewchambers";
     repo = pname;
     rev = version;
-    sha256 = "013k8pr4865f5rp66fjf3a8069kmd29brxv0l20z571gy2kxs5p9";
+    hash = "sha256-uA5XEG9nvqsXg34bqw8k4Rjk5F9bPFSk1HQ4Bv6Ar+I=";
     # sha256 = lib.fakeSha256;
   };
 
-  cargoSha256 = "06ks8crggc7qjqx3llp0mrxn62f6frwfl13fyawg77z0g6wp293y";
+  cargoSha256 = "sha256-5RyCjm8Nxm82AFwIsWChcohrBGVDqc/oWDkrdcvKHV8=";
   # cargoSha256 = lib.fakeSha256;
 
-  nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ libsodium ];
+  nativeBuildInputs = [
+    pkg-config
+  ];
+  buildInputs = [
+    libsodium
+    libiconv
+  ];
 
   meta = with lib; {
     description = "Easy and efficient encrypted backups";
