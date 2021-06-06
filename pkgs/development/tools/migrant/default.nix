9b2d69ca492153b1a96db26061ae60b35eb68f2e
Jason R. McNeil <jason@mcneil.dev>
2021-06-05 20:38:04 -0700

    migrant: update to 0.14.0

--- pkgs/development/tools/migrant/default.nix
+++ pkgs/development/tools/migrant/default.nix
@@ -6,27 +6,31 @@
 , sqlite
 , mariadb
 , darwin
+, libiconv
 }:
 rustPlatform.buildRustPackage rec {
   pname = "migrant";
-  version = "v0.13.0";
+  version = "v0.14.0";
 
   src = fetchFromGitHub {
     owner = "jaemk";
     repo = pname;
-    rev = version;
-    sha256 = "0nbzqx58zcz3bbrdxa75rcbz5s6h7r61l1nc2dbifa5vxf0wbmq2";
+    rev = "846bfd7a5e2e52df92ce619246d4a1331ec5d515"; # untagged v0.14.0
+    sha256 = "1gqlai0wh7rgp62l6w0ipb2mhbmraknya04p3ngplcv1hn3cablj";
     # sha256 = lib.fakeSha256;
   };
 
-  cargoSha256 = "1zhc5568p538kkb2i7q26agiqbqnh3fj23q2h79v30fqyjbrb1ms";
+  cargoSha256 = "0pkih2piii7k524qn1irq1rb0l93pblmggxv6s1d57dn6krvm78z";
   # cargoSha256 = lib.fakeSha256;
 
   buildInputs = [
     postgresql.lib
     sqlite
     mariadb.client
-  ] ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
+  ] ++ lib.optionals stdenv.isDarwin [
+    darwin.apple_sdk.frameworks.Security
+    libiconv
+  ];
 
   cargoBuildFlags = [
     "--features postgres,sqlite,mysql"
