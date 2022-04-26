6b384e6da6c8da73c44affecb1078e9ff43dfdfd
Jason R. McNeil <jason@mcneil.dev>
2022-04-25 23:20:18 -0700

    flake: overlay on pkgs

--- flake.nix
+++ flake.nix
@@ -2,10 +2,8 @@
   description = "dev.mcneil.nix.nixpkgs-staging";
 
   outputs = { self }: {
-    overlay = final: prev: {
-      staging = import ./default.nix {
-        pkgs = prev;
-      };
+    overlay = final: prev: import ./default.nix {
+      pkgs = prev;
     };
   };
 }
