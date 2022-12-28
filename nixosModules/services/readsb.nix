9e19386f5fb95c2f899c88a1aab43924829cf2aa
Jason R. McNeil <jason@mcneil.dev>
2022-12-28 01:15:14 -0700

    services.readsb: First pass at a maybe-working config

--- nixosModules/services/readsb.nix
+++ nixosModules/services/readsb.nix
@@ -8,6 +8,23 @@ in {
   options = {
     services.readsb = {
       enable = mkEnableOption "Enabled ADS-B decoder swiss knife";
+      arguments = mkOption {
+        type = types.listOf str;
+        default = [
+          "--device-type rtlsdr"
+          "--net"
+          "--net-connector feed1.adsbexchange.com,30004,beast_reduce_out,feed2.adsbexchange.com,64004"
+          "--net-heartbeat 60"
+          "--net-ro-size 1280"
+          "--net-ro-interval 0.2"
+          "--net-ro-port 0"
+          "--net-sbs-port 0"
+          "--net-bi-port 30154"
+          "--net-bo-port 0"
+          "--net-ri-port 0"
+          "--write-json-every 1"
+        ];
+      };
     };
   };
 
@@ -17,7 +34,7 @@ in {
       wantedBy = ["multi-user.target"];
       after = ["network-online.target"];
       serviceConfig = {
-        ExecStart = "${pkgs.readsb}/bin/readsb";
+        ExecStart = "${pkgs.readsb}/bin/readsb ${concatStringsSep " " cfg.arguments}";
         RestartSec = 600;
         Restart = "always";
       };
