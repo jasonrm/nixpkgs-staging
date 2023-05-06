f673524a5d7ae64cc78b52ccd79edc8f4ba2c594
Jason R. McNeil <jason@mcneil.dev>
2023-05-06 13:35:33 -0700

    chore: reformat with alejandra

--- pkgs/development/node-packages/overrides.nix
+++ pkgs/development/node-packages/overrides.nix
@@ -1,18 +1,36 @@
 # Do not use overrides in this file to add  `meta.mainProgram` to packges. Use `./main-programs.nix`
 # instead.
-{ pkgs, nodejs }:
-
-let
-  inherit (pkgs)
+{
+  pkgs,
+  nodejs,
+}: let
+  inherit
+    (pkgs)
     stdenv
     lib
     callPackage
     fetchFromGitHub
     fetchurl
     fetchpatch
-    nixosTests;
+    nixosTests
+    ;
 
   since = version: lib.versionAtLeast nodejs.version version;
   before = version: lib.versionOlder nodejs.version version;
 in
-final: prev: { }
+  final: prev: {
+    tslab = prev.tslab.override (oldAttrs: {
+      # loosely based on ijavascript & tedicross
+      preRebuild = ''
+        export npm_config_zmq_external=true
+      '';
+      buildInputs =
+        oldAttrs.buildInputs
+        ++ (with pkgs; [zeromq libsodium czmq])
+        # ++ lib.optionals stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [
+        #   CoreText
+        # ]
+        ;
+      nativeBuildInputs = with pkgs; [pkg-config];
+    });
+  }
