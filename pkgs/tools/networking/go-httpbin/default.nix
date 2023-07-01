127e5f911c33585bbc97da6cd644bd3fde51d12f
Jason R. McNeil <jason@mcneil.dev>
2023-07-01 01:23:27 -0700

    go-httpbin: 2.8.0 -> 2.9.2

--- pkgs/tools/networking/go-httpbin/default.nix
+++ pkgs/tools/networking/go-httpbin/default.nix
@@ -7,18 +7,17 @@
 }:
 buildGoModule rec {
   pname = "go-httpbin";
-  version = "2.8.0";
+  version = "2.9.2";
 
   src = fetchFromGitHub {
     owner = "mccutchen";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-Josk44k6e89g6Qq2iE1d2u5fVz1hHIXx/0lojBzLpQE=";
+    hash = "sha256-dLHsv+DXlDxFdKcPVAlq3O47/1zXPVQx6czK+uzBv8M=";
     # hash = lib.fakeHash;
   };
 
-  vendorHash = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
-  # vendorHash = lib.fakeHash;
+  vendorHash = null;
 
   subPackages = ["cmd/go-httpbin"];
 
