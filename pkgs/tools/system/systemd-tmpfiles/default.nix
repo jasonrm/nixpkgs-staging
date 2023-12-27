63c47f2c459454667bb416421f10115db8f885f0
Jason R. McNeil <jason@mcneil.dev>
2023-12-27 08:24:13 -0700

    systemd-tmpfiles: 0.2.3 -> 0.2.3

--- pkgs/tools/system/systemd-tmpfiles/default.nix
+++ pkgs/tools/system/systemd-tmpfiles/default.nix
@@ -5,17 +5,17 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "systemd-tmpfiles";
-  version = "0.2.2";
+  version = "0.2.3";
 
   src = fetchFromGitHub {
     owner = "jasonrm";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-q6IYS8j92VVLsspMILVercOSWrDDemOa/MjR9lzUABg=";
+    hash = "sha256-4r9idEzl5nIRAsQGPgLhqnZ5ypDyRt8oQMhBCNVuoU0=";
     # hash = lib.fakeHash;
   };
 
-  cargoHash = "sha256-SMDrrVvm9yUQZkZZyvTYWuZyR64B8ZGTkq3+vQPqiDc=";
+  cargoHash = "sha256-jK0V6cofQKV+nPLenFfBPyxNq/uu3J72ur8LMH/nwLI=";
   # cargoHash = lib.fakeHash;
 
   meta = with lib; {
