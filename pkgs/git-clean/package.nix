00d671efbb6db39c31d1cdead6927a0c5e12f38e
Jason R. McNeil <jason@mcneil.dev>
2025-06-04 07:51:44 -0700

    git-clean: fix cargoHash

--- pkgs/git-clean/package.nix
+++ pkgs/git-clean/package.nix
@@ -15,7 +15,7 @@ rustPlatform.buildRustPackage rec {
     # hash = lib.fakeHash;
   };
 
-  cargoHash = "sha256-7ePRJFYjyzjR8TUL1yMMhqI3z76Rw+5cuX6NpyrdidY=";
+  cargoHash = "sha256-/SrAPsXufshZsF+HBGzBbgQOwWv8o7UHtZ5I3PdEWms=";
   # cargoHash = lib.fakeHash;
 
   # Some checks expect to a git repo
@@ -25,6 +25,6 @@ rustPlatform.buildRustPackage rec {
     description = "A Command Line Tool written in Rust for cleaning up local and remote Git branches";
     homepage = "https://github.com/mcasper/git-clean";
     license = licenses.mit;
-    maintainer = ["jason@mcneil.dev"];
+    maintainer = [ "jason@mcneil.dev" ];
   };
 }
