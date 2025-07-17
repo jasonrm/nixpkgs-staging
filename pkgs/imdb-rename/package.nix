0c7361c904e1c1b6ef57501cfe23e77f4ad190f1
Jason R. McNeil <jason@mcneil.dev>
2025-07-17 13:03:09 -0700

    imdb-rename: init at unstable-2024-09-25

---
+++ pkgs/imdb-rename/package.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "imdb-rename";
+  version = "0.0.0-unstable-2024-09-25";
+
+  src = fetchFromGitHub {
+    owner = "BurntSushi";
+    repo = pname;
+    rev = "f4180e5d89b5dd6f83142f7c8a7c8b439ab80ab1";
+    hash = "sha256-xmWF37mt0hwfSwoKFzJbLujHTsM9X1PDhWV9MuTP9/g=";
+  };
+
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-g1kTQvrUvfakFJ3+BmFqFXlsH4uEiZK6KCEa0BG0mSI=";
+
+  meta = with lib; {
+    description = "A command line tool to rename media files based on titles from IMDb";
+    homepage = "https://github.com/BurntSushi/imdb-rename";
+    license = licenses.unlicense;
+    maintainer = [ "jason@mcneil.dev" ];
+  };
+}
