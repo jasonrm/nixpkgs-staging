dbd60cc14b5a3cc9bf2efef4e3c5889b96f68c9d
Jason R. McNeil <jason@mcneil.dev>
2025-07-25 12:41:34 -0700

    arcanum: 0.4.0 -> 0.4.1

--- pkgs/arcanum/package.nix
+++ pkgs/arcanum/package.nix
@@ -5,20 +5,18 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "arcanum";
-  version = "0.4.0";
+  version = "0.4.1";
 
   src = fetchFromGitHub {
     owner = "bitnixdev";
     repo = pname;
     fetchSubmodules = true;
     rev = "v${version}";
-    hash = "sha256-U+qDh7n9jQZTUhaUWbtJlf5JBUDJHKMzgW87mB6tD4c=";
-    # hash = lib.fakeHash;
+    hash = "sha256-FtMEkH0D2msKQ8I2a35RZj1wMa5tD9sLO+YjX1ELzBw=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-jl23aHm/2i2cB8j6KrAeeNavmQQmaLdlGRK+A1HJz3s=";
-  # cargoHash = lib.fakeHash;
+  cargoHash = "sha256-sEw+20qXQQ0F1UNJD6rIgjdYGFOrqg86EiWX1n0rJuU=";
 
   meta = with lib; {
     homepage = "https://github.com/bitnixdev/arcanum";
