09c38947f0b5ad76c797f4cb3d94c482882ac510
Jason R. McNeil <jason@mcneil.dev>
2025-08-31 12:33:30 -0700

    mago: override from unstable with 1.0.0-beta.5

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
     utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
@@ -10,47 +12,48 @@
     };
   };
 
-  outputs =
-    { nixpkgs, utils, ... }:
-    let
-      inherit (nixpkgs.lib) hasSuffix;
-      inherit (nixpkgs.lib.filesystem) listFilesRecursive;
-      inherit (utils.lib) eachDefaultSystem;
+  outputs = {
+    nixpkgs,
+    nixpkgs-unstable,
+    utils,
+    ...
+  }: let
+    inherit (nixpkgs.lib) hasSuffix;
+    inherit (nixpkgs.lib.filesystem) listFilesRecursive;
+    inherit (utils.lib) eachDefaultSystem;
 
-      onlyNix = baseName: (hasSuffix ".nix" baseName);
-      nixosModules = builtins.filter onlyNix (listFilesRecursive ./nixosModules);
+    onlyNix = baseName: (hasSuffix ".nix" baseName);
+    nixosModules = builtins.filter onlyNix (listFilesRecursive ./nixosModules);
 
-      allPackages = import ./pkgs;
-      perSystem = eachDefaultSystem (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          legacyPackages = allPackages {
-            inherit pkgs;
-            lib = pkgs.lib;
-          };
-          devShell = pkgs.mkShell { buildInputs = with pkgs; [ node2nix ]; };
-        }
-      );
-    in
+    allPackages = import ./pkgs;
+    perSystem = eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        legacyPackages = allPackages {
+          inherit pkgs;
+          nixpkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+          lib = pkgs.lib;
+        };
+        devShell = pkgs.mkShell {buildInputs = with pkgs; [node2nix];};
+      }
+    );
+  in
     perSystem
     // {
       nixosModules.default = {
         nixpkgs.overlays = [
           (
             final: prev:
-            allPackages {
-              pkgs = prev;
-              lib = prev.lib;
-            }
+              allPackages {
+                pkgs = prev;
+                lib = prev.lib;
+              }
           )
         ];
         imports = nixosModules;
       };
-      overlays.default =
-        final: prev:
+      overlays.default = final: prev:
         allPackages {
           pkgs = prev;
           lib = prev.lib;
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,9 @@
-{ pkgs, lib, ... }:
-let
+{
+  pkgs,
+  nixpkgs-unstable,
+  lib,
+  ...
+}: let
   inherit (lib) listToAttrs hasSuffix;
   inherit (lib.filesystem) listFilesRecursive;
 
@@ -9,34 +13,57 @@ let
   packages = listToAttrs (
     map (name: {
       name = builtins.baseNameOf (builtins.dirOf name);
-      value = pkgs.callPackage name { };
-    }) packageDefinitions
+      value = pkgs.callPackage name {};
+    })
+    packageDefinitions
   );
 in
-# libOverlay = lib // (import ./lib { inherit pkgs lib; });
-packages
-// {
-  customLib = pkgs.callPackage ./lib { };
+  # libOverlay = lib // (import ./lib { inherit pkgs lib; });
+  packages
+  // {
+    customLib = pkgs.callPackage ./lib {};
 
-  # nodePackages = pkgs.nodePackages.extend (final: prev: (pkgs.callPackage ./node-packages { }));
-
-  yubikey-agent = pkgs.callPackage "${pkgs.path}/pkgs/by-name/yu/yubikey-agent/package.nix" {
-    buildGoModule =
-      args:
-      pkgs.buildGoModule (
-        args
+    mago = nixpkgs-unstable.mago.override {
+      rustPlatform =
+        nixpkgs-unstable.rustPlatform
         // {
-          src = pkgs.fetchFromGitHub {
-            owner = "jasonrm";
-            repo = "yubikey-agent";
-            rev = "9a64bd0ca5b05b8a685a0f83e2a7d0a01f7489dd";
-            # hash = lib.fakeHash;
-            hash = "sha256-Wbm569DqVf7q8zg3lvWdgGaS6IFS9FSxfvD98zc/I14=";
-          };
+          buildRustPackage = args:
+            nixpkgs-unstable.rustPlatform.buildRustPackage (
+              args
+              // rec {
+                pname = "mago";
+                version = "1.0.0-beta.5";
+                src = nixpkgs-unstable.fetchFromGitHub {
+                  owner = "carthage-software";
+                  repo = pname;
+                  tag = version;
+                  hash = "sha256-OjCFUlcslkKBTPW/1W3pmhlO7moRse9t70xDk0jLQbM=";
+                };
 
-          vendorHash = "sha256-ZQCxW+NDeYJofC9/9z8BpcRtBJ5p7hfQfsaX7iRIw5w=";
-          # vendorHash = lib.fakeHash;
-        }
-      );
-  };
-}
+                cargoHash = "sha256-ZfmAjnZuu53SoGRWgWN/JwAMT+d/8WitFwP9+gURKhw=";
+              }
+            );
+        };
+    };
+
+    # nodePackages = pkgs.nodePackages.extend (final: prev: (pkgs.callPackage ./node-packages { }));
+
+    yubikey-agent = pkgs.callPackage "${pkgs.path}/pkgs/by-name/yu/yubikey-agent/package.nix" {
+      buildGoModule = args:
+        pkgs.buildGoModule (
+          args
+          // {
+            src = pkgs.fetchFromGitHub {
+              owner = "jasonrm";
+              repo = "yubikey-agent";
+              rev = "9a64bd0ca5b05b8a685a0f83e2a7d0a01f7489dd";
+              # hash = lib.fakeHash;
+              hash = "sha256-Wbm569DqVf7q8zg3lvWdgGaS6IFS9FSxfvD98zc/I14=";
+            };
+
+            vendorHash = "sha256-ZQCxW+NDeYJofC9/9z8BpcRtBJ5p7hfQfsaX7iRIw5w=";
+            # vendorHash = lib.fakeHash;
+          }
+        );
+    };
+  }
