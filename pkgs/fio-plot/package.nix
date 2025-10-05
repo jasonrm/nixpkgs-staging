e6341b0e55f39fa8a5df0d31bfa3981dadf03a2a
Jason R. McNeil <jason@mcneil.dev>
2025-10-04 23:38:12 -0700

    fio-plot: init at 1.1.16

---
+++ pkgs/fio-plot/package.nix
@@ -0,0 +1,64 @@
+{
+  lib,
+  fio,
+  bc,
+  coreutils,
+  fetchFromGitHub,
+  python3Packages,
+  pyan3,
+  ...
+}:
+python3Packages.buildPythonApplication rec {
+  pname = "fio-plot";
+  version = "1.1.16";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "louwrentius";
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-yN0gVm6ZYEIoh91d+0ohJ9yU+VWwYEq3MoG+WgBrs2Q=";
+  };
+
+  postPatch = ''
+    # Remove the scripts section from setup.py to avoid conflict with entry_points
+    substituteInPlace setup.py \
+      --replace 'scripts=["bin/fio-plot", "bin/bench-fio"],' ""
+  '';
+
+  build-system = with python3Packages; [
+    setuptools
+  ];
+
+  dependencies = with python3Packages; [
+    numpy
+    matplotlib
+    pillow
+    pyparsing
+    rich
+    pyan3
+  ];
+
+  # Make sure runtime dependencies are available in PATH
+  makeWrapperArgs = [
+    "--prefix"
+    "PATH"
+    ":"
+    "${lib.makeBinPath [fio bc coreutils]}"
+  ];
+
+  # Tests require specific benchmark data files
+  doCheck = false;
+
+  meta = with lib; {
+    description = "Create charts from FIO storage benchmark tool output";
+    longDescription = ''
+      Fio-plot generates charts from FIO storage benchmark data. It can process
+      FIO output in JSON format. It can also process FIO log file output (in CSV format).
+      It also includes bench-fio, a benchmark tool to automate benchmarking with FIO.
+    '';
+    homepage = "https://github.com/louwrentius/fio-plot";
+    license = licenses.bsd3;
+    mainProgram = "fio-plot";
+  };
+}
