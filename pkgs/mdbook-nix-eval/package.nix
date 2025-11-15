d40b36d90da58d5c81c4c2fb59c21d57b370db6d
Jason R. McNeil <jason@mcneil.dev>
2025-11-15 16:21:26 -0700

    mdbook-nix-eval: 1.0.1 -> 1.1.0

--- pkgs/mdbook-nix-eval/package.nix
+++ pkgs/mdbook-nix-eval/package.nix
@@ -1,32 +1,25 @@
 {
   lib,
-  stdenv,
   fetchFromGitHub,
-  libiconv,
   rustPlatform,
-  darwin,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "mdbook-nix-eval";
-  version = "1.0.1";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "jasonrm";
     repo = "mdbook-nix-eval";
     rev = "v${version}";
-    sha256 = "0pzzf7jyq378nc27ffdzka676a0jblp259cjqsqcy27z5hzcvnhn";
-    # sha256 = lib.fakeSha256;
+    hash = "sha256-IRBMMUNTDT0FH8TX9MEM+JRxFSiH0WOnOBGwxBbnu2Y=";
   };
 
-  cargoSha256 = "16llg6pcx3lq8szyl2adq0mibvdvdlavz2kv0cs7rzcmdsq356fn";
-  # cargoSha256 = lib.fakeSha256;
-
-  buildInputs = [ libiconv ];
+  cargoHash = "sha256-ZIy5y0wf5UyzwpmiFELN6Nu1i1JoBJ5Np7gUTkixWiQ=";
 
   meta = with lib; {
     description = "A mdbook preprocessor designed to evaluate code blocks containing nix expressions";
     homepage = "https://jasonrm.github.io/mdbook-nix-eval/";
-    license = [ licenses.mpl20 ];
-    # maintainers = [ maintainers.havvy ];
+    license = [licenses.mpl20];
+    maintainers = ["jason@mcneil.dev"];
   };
 }
