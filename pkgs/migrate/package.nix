bf0df415f02305e1056c83076baaf8135271ea55
Jason R. McNeil <jason@mcneil.dev>
2025-12-21 19:37:08 -0700

    migrate: 0.5.0 -> migrate 0.5.1

--- pkgs/migrate/package.nix
+++ pkgs/migrate/package.nix
@@ -5,20 +5,20 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "migrate";
-  version = "0.5.0";
+  version = "0.5.1";
 
   src = fetchFromGitHub {
     owner = "bitnixdev";
     repo = pname;
     fetchSubmodules = true;
     rev = "v${version}";
-    hash = "sha256-7JeeJlPXg4uxdZiWYpcog+YUtWnWdMQbiDx2PJXDMbc=";
+    hash = "sha256-vlXNAvkE82oheH8TxJGv+uSHjjZW9/7J3iIY4iyPzwA=";
   };
 
-  cargoHash = "sha256-ElHfH8rj9UM6jkLszEPgsGqA49R3KZOXk5hbms988zE=";
+  cargoHash = "sha256-81ghlM14g1ks5eoh7coe8a8rwHI1VocIU9uADuUyepo=";
 
   meta = with lib; {
-    description = "Database migration tool for Postgres, MySQL, SQLite, and Amazon Aurora DSQL";
+    description = "Opinionated up-only database migration tool";
     homepage = "https://github.com/bitnixdev/migrate";
     license = licenses.mit;
     maintainer = ["jason@mcneil.dev"];
