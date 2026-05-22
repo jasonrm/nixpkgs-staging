6160d0a8235a2f859155877f35b26afbea160020
Jason R. McNeil <jason@mcneil.dev>
2026-05-22 02:15:54 -0700

    sql-tap: 0.0.5 -> 0.3.0

--- pkgs/sql-tap/package.nix
+++ pkgs/sql-tap/package.nix
@@ -5,13 +5,13 @@
 }:
 buildGoModule rec {
   pname = "sql-tap";
-  version = "0.0.5";
+  version = "0.3.0";
 
   src = fetchFromGitHub {
     owner = "mickamy";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-VlJ6VlIl9rE1bjPLy0uEtqxmrYYtPq2QJ1tVITmRnI8=";
+    hash = "sha256-3c3cG5XPxZXebE27PssHX09AwhsyyHgYsgjh1rmM91k=";
   };
 
   vendorHash = "sha256-zf3NxbxX8X2/ZHBDjbp+cPOviODLTTDrY1LW//3lSDk=";
