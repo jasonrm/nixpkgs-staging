e91fdc92860a5358854b83a6f30bbf278002533f
Jason R. McNeil <jason@mcneil.dev>
2026-05-23 03:10:47 -0700

    names: 0.12.0 -> 0.14.0

--- pkgs/names/package.nix
+++ pkgs/names/package.nix
@@ -1,48 +1,25 @@
 {
-  stdenv,
   lib,
   fetchFromGitHub,
   rustPlatform,
-  postgresql,
-  sqlite,
-  mariadb,
-  darwin,
-  libiconv,
   pkg-config,
-  openssl,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "names";
-  version = "v0.12.0";
+  version = "v0.14.0";
 
   src = fetchFromGitHub {
     owner = "fnichol";
     repo = pname;
     rev = version;
-    hash = "sha256:1ih5alil8193gz9gjgakhly9l072g36a02xc6zh3jdgb1bzgcxrp";
-    # cargoHash = lib.fakeSha256;
+    hash = "sha256-C0JEVTOgxtgvCgSSdYxMCMtAVRU1A7DEczNj4zY8q20=";
   };
 
-  cargoHash = "sha256:0qrx7x3wcmw794yzpgzip4fxhrl48h2syig7wvn6w6rhrl51zl37";
-  # cargoHash = lib.fakeSha256;
+  cargoHash = "sha256-+zNlzo/+CCGzxreDdCj/bjF28euFGuXJspJoBPaG+8E=";
 
   nativeBuildInputs = [
     pkg-config
   ];
-
-  # buildInputs = [
-  #   postgresql.lib
-  #   openssl
-  #   sqlite
-  #   mariadb.client
-  # ] ++ lib.optionals stdenv.isDarwin [
-  #   darwin.apple_sdk.frameworks.Security
-  #   libiconv
-  # ];
-
-  # cargoBuildFlags = [
-  #   "--features postgres,sqlite,mysql"
-  # ];
 
   meta = with lib; {
     description = "Random name generator";
