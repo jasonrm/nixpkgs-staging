cd0aa1d29f3365a952dc5168738ff2e26f0a2f17
Jason R. McNeil <jason@mcneil.dev>
2026-05-23 03:10:18 -0700

    frep: remove commented code

--- pkgs/frep/package.nix
+++ pkgs/frep/package.nix
@@ -11,11 +11,9 @@ buildGoModule rec {
     owner = "subchen";
     repo = pname;
     rev = version;
-    # hash = lib.fakeHash;
     hash = "sha256-opdUjCWVwnHhpFJ0BbjNsQjw5454B7ctK2nP23yBpcQ=";
   };
 
-  # vendorHash = lib.fakeHash;
   vendorHash = "sha256-X2kkqEjWFpelBsPl9O0DMQwSz5myLs5pAnQL8QVeJ3s=";
 
   meta = with lib; {
