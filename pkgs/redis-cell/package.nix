f5faf86d8890cf6c7e040fff65c23137561cc4e9
Jason R. McNeil <jason@mcneil.dev>
2026-05-23 03:10:48 -0700

    redis-cell: 0.4.0 -> 0.5.0

--- pkgs/redis-cell/package.nix
+++ pkgs/redis-cell/package.nix
@@ -5,16 +5,16 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "redis-cell";
-  version = "v0.4.0";
+  version = "v0.5.0";
 
   src = fetchFromGitHub {
     owner = "brandur";
     repo = pname;
     rev = version;
-    sha256 = "sha256-CKdIbyuQlI8UDxVCh5sGO2Ll8h5DQjlkGiuE4SF2JyU=";
+    sha256 = "sha256-ikRUTHw5XMWC8E9jAMM5gRfXnXUhfq2It/WN1u+AAD0=";
   };
 
-  cargoHash = "sha256-48hIsQZdbF30jtlFkm2Ei+9uptmJD7IW+XRujf114mI=";
+  cargoHash = "sha256-qPPH/6wz9KYahyV+4u/T61AiQX/K8YvtgpjLxqi4iNk=";
 
   meta = with lib; {
     description = "A Redis module that provides rate limiting in Redis as a single command.";
